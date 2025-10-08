f1c32bb3b5a196254f5d8011b7f2305814429818
sahaniritesh <sahaniritesh956@gmail.com>
2025-10-08 21:54:39 +0000

    function

---
+++ 03_basics/01_function.js
@@ -0,0 +1,12 @@
+// First Function in  javascript
+
+function sayMyName ()
+{
+    console.log("R");
+    console.log("I");
+    console.log("T");
+    console.log("E");
+    console.log("S");
+    console.log("H");
+}
+ sayMyName()
